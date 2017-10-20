b1c5de9e2e165be7d01682cc96e331f1f05bcd30
Mathew Bramson <mathewbramson@gmail.com>
2017-10-19 20:39:55 -0400

    Clean up code

--- lib/night_in_salem/game_master.ex
+++ lib/night_in_salem/game_master.ex
@@ -8,7 +8,7 @@ defmodule NightInSalem.GameMaster do
   ## Client Functions
 
   def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, :ok, name: GameMaster)
+    GenServer.start_link(__MODULE__, :ok, [name: GameMaster] ++ opts)
   end
 
   def get_state() do
@@ -54,10 +54,10 @@ defmodule NightInSalem.GameMaster do
   end
 
   def handle_call({:add_player, player_name}, _from, state) do
-    if Enum.any?(state, fn user -> user[:name] == player_name end) do
+    if Enum.any?(state, fn user -> user.name == player_name end) do
       {:reply, {:error, :duplicate_name}, state}
     else
-      {:reply, :ok, [new_player(player_name) | state]}
+      {:reply, :ok, [%Player{name: player_name} | state]}
     end
   end
 
@@ -70,54 +70,18 @@ defmodule NightInSalem.GameMaster do
   end
 
   def handle_cast({:become_witch, player_name}, state) do
-    {:noreply, become_witch(state, player_name)}
+    {:noreply, Player.become_witch(state, player_name)}
   end
 
   def handle_cast({:become_constable, player_name}, state) do
-    {:noreply, become_constable(state, player_name)}
+    {:noreply, Player.become_constable(state, player_name)}
   end
 
   def handle_cast({:lose_constable, player_name}, state) do
-    {:noreply, lose_constable(state, player_name)}
+    {:noreply, Player.lose_constable(state, player_name)}
   end
 
   def handle_cast({:become_dead, player_name}, state) do
-    {:noreply, become_dead(state, player_name)}
-  end
-
-  defp become_witch([], _player_name), do: []
-  defp become_witch([%{name: player_name} = user | rest], player_name) do
-    [ Map.put(user, :witch, true) | rest ]
-  end
-  defp become_witch([non_matching_user | rest_of_users], player_name) do
-    [non_matching_user | become_witch(rest_of_users, player_name)]
-  end
-
-  defp become_constable([], _player_name), do: []
-  defp become_constable([%{name: player_name} = user | rest], player_name) do
-    [ Map.put(user, :constable, true) | rest ]
-  end
-  defp become_constable([non_matching_user | rest_of_users], player_name) do
-    [non_matching_user | become_constable(rest_of_users, player_name)]
-  end
-
-  defp lose_constable([], _player_name), do: []
-  defp lose_constable([%{name: player_name} = user | rest], player_name) do
-    [ Map.put(user, :constable, false) | rest ]
-  end
-  defp lose_constable([non_matching_user | rest_of_users], player_name) do
-    [non_matching_user | lose_constable(rest_of_users, player_name)]
-  end
-
-  defp become_dead([], _player_name), do: []
-  defp become_dead([%{name: player_name} = user | rest], player_name) do
-    [ Map.put(user, :dead, true) | rest ]
-  end
-  defp become_dead([non_matching_user | rest_of_users], player_name) do
-    [non_matching_user | become_dead(rest_of_users, player_name)]
-  end
-
-  defp new_player(name) do
-    %Player{name: name}
+    {:noreply, Player.become_dead(state, player_name)}
   end
 end
