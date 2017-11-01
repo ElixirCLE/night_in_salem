ddab5b4524ec50003682908eea6d7c2299991b45
Mike Lustig <mlustig@covermymeds.com>
2017-10-31 22:56:32 -0400

    Simple move toward managing a game session
     - Hook session creation up to the back end
     - Users can now join an existing game session
     - PoC display of total players in a session

--- lib/night_in_salem_web/controllers/game_session_controller.ex
+++ lib/night_in_salem_web/controllers/game_session_controller.ex
@@ -1,5 +1,7 @@
 defmodule NightInSalemWeb.GameSessionController do
   use NightInSalemWeb, :controller
+  alias NightInSalem.GameMasterSupervisor
+  alias NightInSalem.GameMaster
 
   def index(conn, _params) do
     render conn, "index.html"
@@ -10,22 +12,26 @@ defmodule NightInSalemWeb.GameSessionController do
   end
 
   def create(conn, %{ "game_session" => %{ "player_name" => player_name } }) do
+    game_id = GameMasterSupervisor.start_game
+    GameMaster.add_player(game_id, player_name)
     conn
       |> put_session(:player_name, player_name)
-      |> redirect(to: game_session_path(conn, :show, 1))
+      |> redirect(to: game_session_path(conn, :show, game_id))
   end
 
-  def show(conn, %{ "id" => id }) do
-    render conn, "show.html", player_name: get_session(conn, :player_name)
+  def show(conn, %{ "id" => game_id }) do
+    render conn, "show.html", player_name: get_session(conn, :player_name), game_id: game_id, players: GameMaster.get_state(game_id)
   end
 
-  def edit(conn, _params) do
-    render conn, "edit.html"
+  def edit(conn, %{ "id" => game_id }) do
+    render conn, "edit.html", game_id: game_id
   end
 
-  def update(conn, %{ "game_session" => %{ "player_name" => player_name } }) do
+  def update(conn, %{ "game_session" => %{ "player_name" => player_name }, "id" => game_id }) do
+    GameMaster.add_player(game_id, player_name)
     conn
       |> put_session(:player_name, player_name)
-      |> redirect(to: game_session_path(conn, :show, 1))
+      |> put_session(:game_id, game_id)
+      |> redirect(to: game_session_path(conn, :show, game_id))
   end
 end
